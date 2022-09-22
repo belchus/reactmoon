9434b54243bc31df2b73cada7160618755f3cd4c
Camila Arena <arenabelu@gmail.com>
2022-09-21 23:09:05 -0300

    se agrego estilos

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar.js';
 import ItemListContainer from './components/itemlistcontainer.js';
+import Header from './components/header';
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Navbar/>
       <ItemListContainer greeting={"Hola! Somos Moon Accesorios"}/>
     </div>
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import "./navbar.css"
+
+const Header = () => {
+
+    return(
+    <div className="miheader"> 
+    <h1> Moon Accesorios</h1>
+    
+    </div>)
+}
+
+export default Header
