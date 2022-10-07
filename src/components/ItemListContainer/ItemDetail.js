5e1e90ff13db15234665b99017b8768a157a27ed
Camila Arena <arenabelu@gmail.com>
2022-10-06 23:35:14 -0300

    error en img solucionado

--- src/components/ItemListContainer/ItemDetail.js
+++ src/components/ItemListContainer/ItemDetail.js
@@ -2,7 +2,7 @@ const ItemDetail = ( {item}) =>{
     return(
         <div className='fondo'>
             <p className='text'> Informacion sobre : {item.nombre}</p>
-            <img className="ItemDetalle" src={item.imgSrc} alt={item.nombre}/>
+            <img className="ItemDetalle" src={item.imagen} alt={item.nombre}/>
         </div>
     )
 };
